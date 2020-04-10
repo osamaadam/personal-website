2c7e1683006fe53641f2d07439bc674929753a00
Osama Adam <osamaadam98@gmail.com>
2020-04-10 13:06:50 +0200

    experimentation checkpoint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,29 @@ const profile = require("../profile.jpg");
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <img
-        src={profile}
-        alt="Osama Adam"
-        width="200"
-        height="200"
-        className="profile-picture"
-      />
+      <img src={profile} alt="Osama Adam" className="profile-picture" />
+      <div className="home-info">
+        <h1>Osama Adam</h1>
+        <p>Full Stack Web Developer</p>
+      </div>
+      <div className="home-bio">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+          adipisci illo. Tempora quod culpa odio hic eveniet blanditiis, ex
+          soluta architecto recusandae corrupti earum non repudiandae, adipisci
+          doloribus pariatur quo. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Vero nobis facilis aperiam eligendi quasi sit
+          cumque, nihil laboriosam placeat beatae maiores eaque suscipit,
+          exercitationem neque excepturi perferendis eius vel nemo.Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci
+          illo. Tempora quod culpa odio hic eveniet blanditiis, ex soluta
+          architecto recusandae corrupti earum non repudiandae, adipisci
+          doloribus pariatur quo. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Vero nobis facilis aperiam eligendi quasi sit
+          cumque, nihil laboriosam placeat beatae maiores eaque suscipit,
+          exercitationem neque excepturi perferendis eius vel nemo.
+        </p>
+      </div>
     </div>
   );
 };
