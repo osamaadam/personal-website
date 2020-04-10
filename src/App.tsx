681b89a82cc9b2c7dc233c709d9e63a83cde317c
Osama Adam <osamaadam98@gmail.com>
2020-04-10 12:21:12 +0200

    experimenting with background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Home from "./pages/Home";
 
+let repeatingText = "";
+for (let i = 0; i < 500; i++) repeatingText += "Osama Adam ";
+
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <>
+      <h1 className="repeating-background-text">{repeatingText}</h1>
+      <Home />
+    </>
+  );
 };
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,18 @@
 import React from "react";
 import "./home.css";
 
-const profile = require("../profile.jpg") as string;
+const profile = require("../profile.jpg");
 
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <div className="home-info">
-        <img src={profile} alt="Osama Adam" className="home-photo" />
-        <h1 className="home-name">Osama Adam</h1>
-        <p className="home-tag">
-          <span className="highlight">Full Stack Web Developer</span>
-        </p>
-      </div>
-      <div className="home-content">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-          voluptatibus debitis maxime at unde accusantium itaque eligendi
-          molestiae, rerum veniam, dolor porro! Ad, necessitatibus quasi
-          voluptatum dicta officiis praesentium quas. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Inventore deserunt, excepturi dolor
-          dolores sint, accusamus vel fugit eos, consequatur eveniet placeat aut
-          assumenda sed facere quam sapiente hic incidunt saepe?Lorem, ipsum
-          dolor dolores sint, accusamus vel fugit eos, consequatur eveniet
-          placeat aut assumenda sed facere quam sapiente hic incidunt
-          saepe?Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Suscipit, voluptatibus debitis maxime at unde accusantium itaque
-          eligendi molestiae, rerum veniam, dolor porro! Ad, necessitatibus
-          quasi voluptatum dicta officiis praesentium quas. Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Inventore deserunt, excepturi
-          dolor dolores sint, accusamus vel fugit eos, consequatur eveniet
-          placeat aut assumenda sed facere quam sapiente hic incidunt saepe?
-        </p>
-      </div>
+      <img
+        src={profile}
+        alt="Osama Adam"
+        width="200"
+        height="200"
+        className="profile-picture"
+      />
     </div>
   );
 };
