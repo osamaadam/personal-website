10fedbfcf0ed77ec1e1e73aeb5b76f2c085b9755
Osama Adam <osamaadam98@gmail.com>
2020-04-10 17:11:40 +0200

    probably the basic structure for what is to come

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,29 +5,32 @@ const profile = require("../profile.jpg");
 
 const Home = () => {
   return (
-    <div className="home-wrapper">
-      <img src={profile} alt="Osama Adam" className="profile-picture" />
-      <div className="home-info">
-        <h1>Osama Adam</h1>
-        <p>Full Stack Web Developer</p>
-      </div>
-      <div className="home-bio">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
-          adipisci illo. Tempora quod culpa odio hic eveniet blanditiis, ex
-          soluta architecto recusandae corrupti earum non repudiandae, adipisci
-          doloribus pariatur quo. Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Vero nobis facilis aperiam eligendi quasi sit
-          cumque, nihil laboriosam placeat beatae maiores eaque suscipit,
-          exercitationem neque excepturi perferendis eius vel nemo.Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci
-          illo. Tempora quod culpa odio hic eveniet blanditiis, ex soluta
-          architecto recusandae corrupti earum non repudiandae, adipisci
-          doloribus pariatur quo. Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Vero nobis facilis aperiam eligendi quasi sit
-          cumque, nihil laboriosam placeat beatae maiores eaque suscipit,
-          exercitationem neque excepturi perferendis eius vel nemo.
-        </p>
+    <div className="home-container">
+      <div className="home-profile-paper">
+        <img src={profile} alt="Osama Adam" className="profile-picture" />
+        <div className="home-info">
+          <h1>Osama Adam</h1>
+          <p>Full Stack Web Developer</p>
+        </div>
+        <div className="home-bio">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+            adipisci illo. Tempora quod culpa odio hic eveniet blanditiis, ex
+            soluta architecto recusandae corrupti earum non repudiandae,
+            adipisci doloribus pariatur quo. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Vero nobis facilis aperiam eligendi
+            quasi sit cumque, nihil laboriosam placeat beatae maiores eaque
+            suscipit, exercitationem neque excepturi perferendis eius vel
+            nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem, adipisci illo. Tempora quod culpa odio hic eveniet
+            blanditiis, ex soluta architecto recusandae corrupti earum non
+            repudiandae, adipisci doloribus pariatur quo. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Vero nobis facilis aperiam
+            eligendi quasi sit cumque, nihil laboriosam placeat beatae maiores
+            eaque suscipit, exercitationem neque excepturi perferendis eius vel
+            nemo.
+          </p>
+        </div>
       </div>
     </div>
   );
